3a285d3512b2d504d660a9749080de46b849ef2b
Mazino <sbbharath81@gmail.com>
2026-06-22 14:11:59 +0530

    Enhance logging for model loading in summarization
    Added print statements to indicate module loading and successful model loading.

--- 7.py
+++ 7.py
@@ -1,18 +1,12 @@
 from transformers import pipeline
-
+print("loading modules")
 summarizer = pipeline("summarization")
-
+print("Model loaded successfully.\n")
 text = """
 Artificial Intelligence is transforming industries. 
 It helps in automation, healthcare, education, and robotics. 
 AI improves efficiency and decision-making.
 """
-
 summary = summarizer(text)
-
 print(summary)
-# # Step 3: Generate summary
-# summary = summarizer(text)
 
-# # Step 4: Output
-# print(summary[0]['summary_text'])
